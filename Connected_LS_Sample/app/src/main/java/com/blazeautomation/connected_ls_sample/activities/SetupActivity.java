7316a20018c61fea7cb8fdd303dc388fdb139b68
harsukhbir <harsukhbir.impinge@gmail.com>
2020-09-29 21:43:43 +0530

    PowerHubUi

---
+++ Connected_LS_Sample/app/src/main/java/com/blazeautomation/connected_ls_sample/activities/SetupActivity.java
@@ -0,0 +1,16 @@
+package com.blazeautomation.connected_ls_sample.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.blazeautomation.connected_ls_sample.R;
+
+public class SetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+    }
+}
